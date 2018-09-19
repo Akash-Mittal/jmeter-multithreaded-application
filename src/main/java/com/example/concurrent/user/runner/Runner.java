9dc42d9fa944dff96081ed5ca03ab521df6b5930
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-09-20 02:54:26 +0530

    Changes:1.) Misc

--- src/main/java/com/example/concurrent/user/runner/Runner.java
+++ src/main/java/com/example/concurrent/user/runner/Runner.java
@@ -7,8 +7,8 @@ import java.util.concurrent.Executors;
 public interface Runner extends Runnable {
     ExecutorService pool = Executors.newCachedThreadPool();
     Properties props = System.getProperties();
-    Integer numberOfUsers = Integer.valueOf(props.getProperty("wallet.users", "2"));
-    Integer numberOfRequests = Integer.valueOf(props.getProperty("wallet.threads", "2"));
+    Integer numberOfUsers = Integer.valueOf(props.getProperty("wallet.users", "10"));
+    Integer numberOfRequests = Integer.valueOf(props.getProperty("wallet.threads", "20"));
     Integer numberOfRounds = Integer.valueOf(props.getProperty("wallet.rounds", "2"));
 
 }
